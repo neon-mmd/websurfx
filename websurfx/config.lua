4b70a74ff6196a00d8878333ab4b56b065427230
zhou fan <1247714429@qq.com>
2023-05-23 17:30:36 +0800

    Update websurfx/config.lua
    Co-authored-by: neon_arch <mustafadhuleb53@gmail.com>

--- websurfx/config.lua
+++ websurfx/config.lua
@@ -20,6 +20,6 @@ theme = "simple" -- the theme name which should be used for the website
 -- Caching
 redis_connection_url = "redis://127.0.0.1:8082" -- redis connection url address on which the client should connect on.
 
-production_use = false -- whether to use production mode or not
+production_use = false -- whether to use production mode or not (in other words this option should be used if it is to be used to host it on the server to provide a service to a large number of users)
 -- if production_use is set to true
-  -- there will be a random delay before sending the request to the search engines, this is to prevent the search engines from blocking the ip address
+  -- There will be a random delay before sending the request to the search engines, this is to prevent DDoSing the upstream search engines from a large number of simultaneous requests.
