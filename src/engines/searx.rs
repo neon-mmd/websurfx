9a4cf949e42d13882c1f428b5d01f6a35f299e59
xffxff <1247714429@qq.com>
2023-06-14 20:42:30 +0800

    improve error handling by using `error-stack` crate

--- src/engines/searx.rs
+++ src/engines/searx.rs
@@ -8,7 +8,8 @@ use std::collections::HashMap;
 
 use crate::search_results_handler::aggregation_models::RawSearchResult;
 
-use super::engine_models::EngineErrorKind;
+use super::engine_models::EngineError;
+use error_stack::{IntoReport, Report, Result, ResultExt};
 
 /// This function scrapes results from the upstream engine duckduckgo and puts all the scraped
 /// results like title, visiting_url (href in html),engine (from which engine it was fetched from)
@@ -31,43 +32,76 @@ pub async fn results(
     query: &str,
     page: u32,
     user_agent: &str,
-) -> Result<HashMap<String, RawSearchResult>, EngineErrorKind> {
+) -> Result<HashMap<String, RawSearchResult>, EngineError> {
     // Page number can be missing or empty string and so appropriate handling is required
     // so that upstream server recieves valid page number.
     let url: String = format!("https://searx.work/search?q={query}&pageno={page}");
 
     // initializing headers and adding appropriate headers.
     let mut header_map = HeaderMap::new();
-    header_map.insert(USER_AGENT, user_agent.parse()?);
-    header_map.insert(REFERER, "https://google.com/".parse()?);
-    header_map.insert(CONTENT_TYPE, "application/x-www-form-urlencoded".parse()?);
-    header_map.insert(COOKIE, "categories=general; language=auto; locale=en; autocomplete=duckduckgo; image_proxy=1; method=POST; safesearch=2; theme=simple; results_on_new_tab=1; doi_resolver=oadoi.org; simple_style=auto; center_alignment=1; query_in_title=1; infinite_scroll=0; disabled_engines=; enabled_engines=\"archive is__general\\054yep__general\\054curlie__general\\054currency__general\\054ddg definitions__general\\054wikidata__general\\054duckduckgo__general\\054tineye__general\\054lingva__general\\054startpage__general\\054yahoo__general\\054wiby__general\\054marginalia__general\\054alexandria__general\\054wikibooks__general\\054wikiquote__general\\054wikisource__general\\054wikiversity__general\\054wikivoyage__general\\054dictzone__general\\054seznam__general\\054mojeek__general\\054naver__general\\054wikimini__general\\054brave__general\\054petalsearch__general\\054goo__general\"; disabled_plugins=; enabled_plugins=\"searx.plugins.hostname_replace\\054searx.plugins.oa_doi_rewrite\\054searx.plugins.vim_hotkeys\"; tokens=; maintab=on; enginetab=on".parse()?);
+    header_map.insert(
+        USER_AGENT,
+        user_agent
+            .parse()
+            .into_report()
+            .change_context(EngineError::UnexpectedError)?,
+    );
+    header_map.insert(
+        REFERER,
+        "https://google.com/"
+            .parse()
+            .into_report()
+            .change_context(EngineError::UnexpectedError)?,
+    );
+    header_map.insert(
+        CONTENT_TYPE,
+        "application/x-www-form-urlencoded"
+            .parse()
+            .into_report()
+            .change_context(EngineError::UnexpectedError)?,
+    );
+    header_map.insert(COOKIE, "categories=general; language=auto; locale=en; autocomplete=duckduckgo; image_proxy=1; method=POST; safesearch=2; theme=simple; results_on_new_tab=1; doi_resolver=oadoi.org; simple_style=auto; center_alignment=1; query_in_title=1; infinite_scroll=0; disabled_engines=; enabled_engines=\"archive is__general\\054yep__general\\054curlie__general\\054currency__general\\054ddg definitions__general\\054wikidata__general\\054duckduckgo__general\\054tineye__general\\054lingva__general\\054startpage__general\\054yahoo__general\\054wiby__general\\054marginalia__general\\054alexandria__general\\054wikibooks__general\\054wikiquote__general\\054wikisource__general\\054wikiversity__general\\054wikivoyage__general\\054dictzone__general\\054seznam__general\\054mojeek__general\\054naver__general\\054wikimini__general\\054brave__general\\054petalsearch__general\\054goo__general\"; disabled_plugins=; enabled_plugins=\"searx.plugins.hostname_replace\\054searx.plugins.oa_doi_rewrite\\054searx.plugins.vim_hotkeys\"; tokens=; maintab=on; enginetab=on".parse().into_report().change_context(EngineError::UnexpectedError)?);
 
     // fetch the html from upstream searx instance engine
     let results: String = reqwest::Client::new()
         .get(url)
         .headers(header_map) // add spoofed headers to emulate human behaviours.
         .send()
-        .await?
+        .await
+        .into_report()
+        .change_context(EngineError::RequestError)?
         .text()
-        .await?;
+        .await
+        .into_report()
+        .change_context(EngineError::RequestError)?;
 
     let document: Html = Html::parse_document(&results);
 
-    let no_result: Selector = Selector::parse("#urls>.dialog-error>p")?;
+    let no_result: Selector = Selector::parse("#urls>.dialog-error>p")
+        .map_err(|_| Report::new(EngineError::UnexpectedError))
+        .attach_printable_lazy(|| format!("invalid CSS selector: {}", "#urls>.dialog-error>p"))?;
 
     if let Some(no_result_msg) = document.select(&no_result).nth(1) {
         if no_result_msg.inner_html()
             == "we didn't find any results. Please use another query or search in more categories"
         {
-            return Err(EngineErrorKind::EmptyResultSet);
+            return Err(Report::new(EngineError::EmptyResultSet));
         }
     }
 
-    let results: Selector = Selector::parse(".result")?;
-    let result_title: Selector = Selector::parse("h3>a")?;
-    let result_url: Selector = Selector::parse("h3>a")?;
-    let result_desc: Selector = Selector::parse(".content")?;
+    let results: Selector = Selector::parse(".result")
+        .map_err(|_| Report::new(EngineError::UnexpectedError))
+        .attach_printable_lazy(|| format!("invalid CSS selector: {}", ".result"))?;
+    let result_title: Selector = Selector::parse("h3>a")
+        .map_err(|_| Report::new(EngineError::UnexpectedError))
+        .attach_printable_lazy(|| format!("invalid CSS selector: {}", "h3>a"))?;
+    let result_url: Selector = Selector::parse("h3>a")
+        .map_err(|_| Report::new(EngineError::UnexpectedError))
+        .attach_printable_lazy(|| format!("invalid CSS selector: {}", "h3>a"))?;
+
+    let result_desc: Selector = Selector::parse(".content")
+        .map_err(|_| Report::new(EngineError::UnexpectedError))
+        .attach_printable_lazy(|| format!("invalid CSS selector: {}", ".content"))?;
 
     // scrape all the results from the html
     Ok(document
