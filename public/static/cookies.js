3be0c65109db6760084736241fc75670ab35adcf
neon_arch <mustafadhuleb53@gmail.com>
2023-06-19 19:38:45 +0300

    ✨ feat: use ternary condition statement and add error-handling

--- public/static/cookies.js
+++ public/static/cookies.js
@@ -1,16 +1,17 @@
 // This function is executed when any page on the website finsihes loading and
-// this function retrieves the cookies if it is present on the user's machine. 
-// If it is available then the saved cookies is display in the cookies tab 
+// this function retrieves the cookies if it is present on the user's machine.
+// If it is available then the saved cookies is display in the cookies tab
 // otherwise an appropriate message is displayed if it is not available.
 document.addEventListener(
   'DOMContentLoaded',
   () => {
-    let cookie = decodeURIComponent(document.cookie)
-    if (cookie !== '') {
-      document.querySelector('.cookies input').value = cookie
-    } else {
+    try {
+      let cookie = decodeURIComponent(document.cookie)
       document.querySelector('.cookies input').value =
-        'No cookies has been saved on your system'
+        cookie !== '' ? cookie : 'No cookies have been saved on your system'
+    } catch (error) {
+      console.error('Error decoding cookie:', error)
+      document.querySelector('.cookies input').value = 'Error decoding cookie'
     }
   },
   false
