3af1d5954991d2c2a9292faec825b5bf27641783
alamin655 <mdalamin655@outlook.com>
2023-09-21 16:28:24 +0530

    Update config.lua

--- websurfx/config.lua
+++ websurfx/config.lua
@@ -1,17 +1,17 @@
 -- ### General ###
 logging = true -- an option to enable or disable logs.
-debug = true -- an option to enable or disable debug mode.
-threads = 5 -- the amount of threads that the app will use to run (the value should be greater than 0).
+debug = false -- an option to enable or disable debug mode.
+threads = 10 -- the amount of threads that the app will use to run (the value should be greater than 0).
 
 -- ### Server ###
 port = "8080" -- port on which server should be launched
 binding_ip = "0.0.0.0" --ip address on the which server should be launched.
-production_use = true -- whether to use production mode or not (in other words this option should be used if it is to be used to host it on the server to provide a service to a large number of users (more than one))
+production_use = false -- whether to use production mode or not (in other words this option should be used if it is to be used to host it on the server to provide a service to a large number of users (more than one))
 -- if production_use is set to true
 -- There will be a random delay before sending the request to the search engines, this is to prevent DDoSing the upstream search engines from a large number of simultaneous requests.
 request_timeout = 30 -- timeout for the search requests sent to the upstream search engines to be fetched (value in seconds).
 rate_limiter = {
-	number_of_requests = 20, -- The number of request that are allowed within a provided time limit.
+	number_of_requests = 50, -- The number of request that are allowed within a provided time limit.
 	time_limit = 3, -- The time limit in which the quantity of requests that should be accepted.
 }
 
@@ -24,7 +24,7 @@ rate_limiter = {
 -- 3 - High
 -- 4 - Aggressive
 -- }}
-safe_search = 0
+safe_search = 2
 
 -- ### Website ###
 -- The different colorschemes provided are:
@@ -46,7 +46,7 @@ colorscheme = "catppuccin-mocha" -- the colorscheme name which should be used fo
 theme = "simple" -- the theme name which should be used for the website
 
 -- ### Caching ###
--- redis_url = "redis://127.0.0.1:8082" -- redis connection url address on which the client should connect on.
+redis_url = "redis://127.0.0.1:8082" -- redis connection url address on which the client should connect on.
 
 -- ### Search Engines ###
 upstream_search_engines = {
