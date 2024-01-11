1071211deea7797c308dce95793e723b0ffcc02c
Spencerjibz <spencernajib2@gmail.com>
2024-01-11 06:04:45 +0000

    fixed type complexity and add docs for types

--- src/cache/encryption.rs
+++ src/cache/encryption.rs
@@ -1,6 +1,3 @@
-/// --- Add appropriate documentation for this type ---
-type ChaChaCoreStreamCipher = ChaChaPoly1305<StreamCipherCoreWrapper<ChaChaCore<UInt<UInt<UInt<UInt<UTerm, B1>, B0>, B1>, B0>>>;
-
 use chacha20poly1305::{
     consts::{B0, B1},
     ChaChaPoly1305,
@@ -16,11 +13,13 @@ use chacha20::{
     ChaChaCore,
 };
 
-/// OUR CIPHER, only initialised once
-pub static CIPHER: OnceLock<ChaChaCoreStreamCipher>,
-    >,
-> = OnceLock::new();
-/// OUR ENCRYPTION KEY
-pub static ENCRYPTION_KEY: OnceLock<
-    GenericArray<u8, UInt<UInt<UInt<UInt<UTerm, B1>, B1>, B0>, B0>>,
-> = OnceLock::new();
+/// The ChaCha20 core wrapped in a stream cipher for use in ChaCha20-Poly1305 authenticated encryption.
+type StreamCipherCoreWrapperType =
+    StreamCipherCoreWrapper<ChaChaCore<UInt<UInt<UInt<UInt<UTerm, B1>, B0>, B1>, B0>>>;
+/// Our ChaCha20-Poly1305 cipher instance, lazily initialized.
+pub static CIPHER: OnceLock<ChaChaPoly1305<StreamCipherCoreWrapperType>> = OnceLock::new();
+
+/// The type alias for our encryption key, a 32-byte array.
+type GenericArrayType = GenericArray<u8, UInt<UInt<UInt<UInt<UTerm, B1>, B1>, B0>, B0>>;
+/// Our encryption key, lazily initialized.
+pub static ENCRYPTION_KEY: OnceLock<GenericArrayType> = OnceLock::new();
