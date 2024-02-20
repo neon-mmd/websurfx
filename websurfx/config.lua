6e2a5861d887c6a0b95ebd3b7aa987f181843d11
ddotthomas <ddotthomas@gmail.com>
2024-02-20 12:11:33 -0700

    Change adaptive window config name
    Co-authored-by: neon_arch <mustafadhuleb53@gmail.com>

--- websurfx/config.lua
+++ websurfx/config.lua
@@ -14,8 +14,8 @@ rate_limiter = {
 	number_of_requests = 20, -- The number of request that are allowed within a provided time limit.
 	time_limit = 3, -- The time limit in which the quantity of requests that should be accepted.
 }
--- Set whether the server will use an adaptive/dynamic TCP window size
-adaptive_window = true
+-- Set whether the server will use an adaptive/dynamic HTTPS window size
+https_adaptive_window_size = false
 
 -- ### Search ###
 -- Filter results based on different levels. The levels provided are:
