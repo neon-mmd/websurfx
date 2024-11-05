e11e1318cd6bb88564529cd5b2180fe60db6d9c8
neon_arch <mustafadhuleb53@gmail.com>
2024-11-05 23:39:20 +0300

    🚨 fix: make cargo checks happy (#523)

--- src/config/parser.rs
+++ src/config/parser.rs
@@ -134,6 +134,7 @@ impl Config {
         });
 
         Ok(Config {
+            operating_system_tls_certificates: globals.get::<_, bool>("operating_system_tls_certificates")?,
             port: globals.get::<_, u16>("port")?,
             binding_ip: globals.get::<_, String>("binding_ip")?,
             style: Style::new(
